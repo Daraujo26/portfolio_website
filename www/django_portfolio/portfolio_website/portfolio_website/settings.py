d883fe45a23259632d579220714276b31da01e98
Daraujo26 <84485192+Daraujo26@users.noreply.github.com>
2023-11-13 11:06:15 -0500

    Update settings.py

--- www/django_portfolio/portfolio_website/portfolio_website/settings.py
+++ www/django_portfolio/portfolio_website/portfolio_website/settings.py
@@ -24,7 +24,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_e9^ocwfp#g4l$no2ij5)0$2%-4t55&h-5wcvi4izow1a5cjn%'
+SECRET_KEY = '*******************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
