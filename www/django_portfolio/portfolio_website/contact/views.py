49f4dd5e667762dcb9812547229bc4a0a4510640
Daraujo26 <David.araujo2130@gmail.com>
2024-03-25 13:03:34 -0400

    Adding contact form backend

--- www/django_portfolio/portfolio_website/contact/views.py
+++ www/django_portfolio/portfolio_website/contact/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from .models import Contact
+from django.core.mail import send_mail
 
-# Create your views here.
+@csrf_exempt 
+@require_POST
+def submit_contact_form(request):
+    data = request.POST
+
+    # Save submission to the database
+    contact = Contact.objects.create(
+        name=data.get('name'),
+        email=data.get('email'),
+        subject=data.get('subject'),
+        message=data.get('message')
+    )
+
+    # Send an email notification
+    send_mail(
+        subject=f"New contact form submission from {contact.name}",
+        message=contact.message,
+        from_email=contact.email,
+        recipient_list=['contact.davidaraujo@gmail.com'],
+    )
+
+    return JsonResponse({"success": "Your message has been sent successfully."})
